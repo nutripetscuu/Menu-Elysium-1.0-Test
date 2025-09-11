2db5a53fb440883c91566d17f372b3108892e22b
Nutri Pet <nutri.petscuu@gmail.com>
2025-09-11 20:07:56 +0000

    there are no visible changes, lets try that again. please not how you pr

--- src/components/promo-banner.tsx
+++ src/components/promo-banner.tsx
@@ -6,7 +6,7 @@ export function PromoBanner() {
   const promoImage = PlaceHolderImages.find(p => p.id === 'promo-banner');
 
   return (
-    <section className="relative h-[150px] w-full md:h-[180px]">
+    <section className="relative h-48 w-full md:h-64">
       {promoImage && (
         <Image
           src={promoImage.imageUrl}
